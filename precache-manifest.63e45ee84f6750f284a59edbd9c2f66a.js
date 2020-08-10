6dcfa9087813f4969fbb39e55b76da99a4b1aeb9
Anchello <bob.m@mail.ru>
2020-08-10 20:54:32 +0200

    Updates

---
+++ precache-manifest.63e45ee84f6750f284a59edbd9c2f66a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7f3bb2325740eb3ff045d36c6aee59",
+    "url": "/aso-stats/index.html"
+  },
+  {
+    "revision": "51e26e805ef311ddc0cc",
+    "url": "/aso-stats/static/css/4.28cb9192.chunk.css"
+  },
+  {
+    "revision": "c01e4966a49fc9ec831c",
+    "url": "/aso-stats/static/css/main.3c3a1c34.chunk.css"
+  },
+  {
+    "revision": "22778210bfc56a29efe3",
+    "url": "/aso-stats/static/js/2.4b356944.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aso-stats/static/js/2.4b356944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4b5c344780f5e462d0",
+    "url": "/aso-stats/static/js/3.1f6f4ca5.chunk.js"
+  },
+  {
+    "revision": "51e26e805ef311ddc0cc",
+    "url": "/aso-stats/static/js/4.e9cdfda4.chunk.js"
+  },
+  {
+    "revision": "c01e4966a49fc9ec831c",
+    "url": "/aso-stats/static/js/main.f0d0f403.chunk.js"
+  },
+  {
+    "revision": "66d787e81dae569a5ab2",
+    "url": "/aso-stats/static/js/runtime-main.330dca7a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/aso-stats/precache-manifest.63e45ee84f6750f284a59edbd9c2f66a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aso-stats/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
